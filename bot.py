e81a3216371d2dc86805e9729720639a591f111e
GitKodland <125306926+GitKodland@users.noreply.github.com>
2024-11-19 11:21:43 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -12,24 +12,24 @@ class PersistentView(discord.ui.View):
         super().__init__(timeout=None)
         self.owner = owner
 
-    @discord.ui.button(label="Получить ответ", style=discord.ButtonStyle.primary, custom_id="text_ans")
+    @discord.ui.button(label="Cevap Al", style=discord.ButtonStyle.primary, custom_id="text_ans")
     async def text_ans_button(self, interaction: discord.Interaction, button: discord.ui.Button):
         obj = TextAnalysis.memory[self.owner][-1]
         await interaction.response.send_message(obj.response, ephemeral=True)
 
-    @discord.ui.button(label="Перевести сообщение", style=discord.ButtonStyle.secondary, custom_id="text_translate")
+    @discord.ui.button(label="Mesajı çevirin", style=discord.ButtonStyle.secondary, custom_id="text_translate")
     async def text_translate_button(self, interaction: discord.Interaction, button: discord.ui.Button):
         obj = TextAnalysis.memory[self.owner][-1]
         await interaction.response.send_message(obj.translation, ephemeral=True)
 
 @bot.event
 async def on_ready():
-    print(f'Logged in as {bot.user}')
+    print(f'{bot.user} olarak giriş yapıldı}')
 
 @bot.command(name="start")
 async def start(ctx, *, text: str):
     TextAnalysis(text, ctx.author.name)
     view = PersistentView(ctx.author.name)
-    await ctx.send("Я получил твое сообщение! Что ты хочешь с ним сделать?", view=view)
+    await ctx.send("Mesajını aldım! Bununla ne yapmak istiyorsun?", view=view)
 
 bot.run(token)
